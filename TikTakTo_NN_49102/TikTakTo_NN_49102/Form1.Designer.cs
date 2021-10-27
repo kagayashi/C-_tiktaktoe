94e47c219ddd90c8f27a5daaddfac5968430f16c
kagayashi <73599366+kagayashi@users.noreply.github.com>
2021-10-27 21:56:10 +0300

    Add files via upload

---
+++ TikTakTo_NN_49102/TikTakTo_NN_49102/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace TikTakTo_NN_49102
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.b11 = new System.Windows.Forms.Button();
+			this.b12 = new System.Windows.Forms.Button();
+			this.b13 = new System.Windows.Forms.Button();
+			this.b21 = new System.Windows.Forms.Button();
+			this.b22 = new System.Windows.Forms.Button();
+			this.b23 = new System.Windows.Forms.Button();
+			this.b31 = new System.Windows.Forms.Button();
+			this.b32 = new System.Windows.Forms.Button();
+			this.b33 = new System.Windows.Forms.Button();
+			this.Reset = new System.Windows.Forms.Button();
+			this.grazaX = new System.Windows.Forms.Button();
+			this.grazaO = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label_chat = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// b11
+			// 
+			this.b11.Enabled = false;
+			this.b11.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b11.Location = new System.Drawing.Point(116, 27);
+			this.b11.Name = "b11";
+			this.b11.Size = new System.Drawing.Size(50, 50);
+			this.b11.TabIndex = 0;
+			this.b11.UseVisualStyleBackColor = true;
+			this.b11.Click += new System.EventHandler(this.button_click);
+			// 
+			// b12
+			// 
+			this.b12.Enabled = false;
+			this.b12.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b12.Location = new System.Drawing.Point(172, 27);
+			this.b12.Name = "b12";
+			this.b12.Size = new System.Drawing.Size(50, 50);
+			this.b12.TabIndex = 1;
+			this.b12.UseVisualStyleBackColor = true;
+			this.b12.Click += new System.EventHandler(this.button_click);
+			// 
+			// b13
+			// 
+			this.b13.Enabled = false;
+			this.b13.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b13.Location = new System.Drawing.Point(228, 27);
+			this.b13.Name = "b13";
+			this.b13.Size = new System.Drawing.Size(50, 50);
+			this.b13.TabIndex = 2;
+			this.b13.UseVisualStyleBackColor = true;
+			this.b13.Click += new System.EventHandler(this.button_click);
+			// 
+			// b21
+			// 
+			this.b21.Enabled = false;
+			this.b21.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b21.Location = new System.Drawing.Point(116, 83);
+			this.b21.Name = "b21";
+			this.b21.Size = new System.Drawing.Size(50, 50);
+			this.b21.TabIndex = 3;
+			this.b21.UseVisualStyleBackColor = true;
+			this.b21.Click += new System.EventHandler(this.button_click);
+			// 
+			// b22
+			// 
+			this.b22.Enabled = false;
+			this.b22.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b22.Location = new System.Drawing.Point(172, 83);
+			this.b22.Name = "b22";
+			this.b22.Size = new System.Drawing.Size(50, 50);
+			this.b22.TabIndex = 4;
+			this.b22.UseVisualStyleBackColor = true;
+			this.b22.Click += new System.EventHandler(this.button_click);
+			// 
+			// b23
+			// 
+			this.b23.Enabled = false;
+			this.b23.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b23.Location = new System.Drawing.Point(228, 83);
+			this.b23.Name = "b23";
+			this.b23.Size = new System.Drawing.Size(50, 50);
+			this.b23.TabIndex = 5;
+			this.b23.UseVisualStyleBackColor = true;
+			this.b23.Click += new System.EventHandler(this.button_click);
+			// 
+			// b31
+			// 
+			this.b31.Enabled = false;
+			this.b31.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b31.Location = new System.Drawing.Point(116, 139);
+			this.b31.Name = "b31";
+			this.b31.Size = new System.Drawing.Size(50, 50);
+			this.b31.TabIndex = 6;
+			this.b31.UseVisualStyleBackColor = true;
+			this.b31.Click += new System.EventHandler(this.button_click);
+			// 
+			// b32
+			// 
+			this.b32.Enabled = false;
+			this.b32.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b32.Location = new System.Drawing.Point(172, 139);
+			this.b32.Name = "b32";
+			this.b32.Size = new System.Drawing.Size(50, 50);
+			this.b32.TabIndex = 7;
+			this.b32.UseVisualStyleBackColor = true;
+			this.b32.Click += new System.EventHandler(this.button_click);
+			// 
+			// b33
+			// 
+			this.b33.Enabled = false;
+			this.b33.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.b33.Location = new System.Drawing.Point(228, 139);
+			this.b33.Name = "b33";
+			this.b33.Size = new System.Drawing.Size(50, 50);
+			this.b33.TabIndex = 8;
+			this.b33.UseVisualStyleBackColor = true;
+			this.b33.Click += new System.EventHandler(this.button_click);
+			// 
+			// Reset
+			// 
+			this.Reset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.Reset.Enabled = false;
+			this.Reset.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Reset.Location = new System.Drawing.Point(291, 279);
+			this.Reset.Name = "Reset";
+			this.Reset.Size = new System.Drawing.Size(97, 49);
+			this.Reset.TabIndex = 9;
+			this.Reset.Text = "Reset";
+			this.Reset.UseVisualStyleBackColor = true;
+			this.Reset.Click += new System.EventHandler(this.Reset_Click);
+			// 
+			// grazaX
+			// 
+			this.grazaX.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.grazaX.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.grazaX.Location = new System.Drawing.Point(12, 279);
+			this.grazaX.Name = "grazaX";
+			this.grazaX.Size = new System.Drawing.Size(111, 24);
+			this.grazaX.TabIndex = 10;
+			this.grazaX.Text = "Gracz za X";
+			this.grazaX.UseVisualStyleBackColor = true;
+			this.grazaX.Click += new System.EventHandler(this.grazaX_Click);
+			// 
+			// grazaO
+			// 
+			this.grazaO.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.grazaO.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.grazaO.Location = new System.Drawing.Point(12, 309);
+			this.grazaO.Name = "grazaO";
+			this.grazaO.Size = new System.Drawing.Size(111, 24);
+			this.grazaO.TabIndex = 11;
+			this.grazaO.Text = "Gracz za O";
+			this.grazaO.UseVisualStyleBackColor = true;
+			this.grazaO.Click += new System.EventHandler(this.grazaO_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 214);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(25, 15);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "PC:";
+			// 
+			// label_chat
+			// 
+			this.label_chat.AutoSize = true;
+			this.label_chat.Location = new System.Drawing.Point(43, 214);
+			this.label_chat.Name = "label_chat";
+			this.label_chat.Size = new System.Drawing.Size(0, 15);
+			this.label_chat.TabIndex = 13;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(400, 340);
+			this.Controls.Add(this.label_chat);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.grazaO);
+			this.Controls.Add(this.grazaX);
+			this.Controls.Add(this.Reset);
+			this.Controls.Add(this.b33);
+			this.Controls.Add(this.b32);
+			this.Controls.Add(this.b31);
+			this.Controls.Add(this.b23);
+			this.Controls.Add(this.b22);
+			this.Controls.Add(this.b21);
+			this.Controls.Add(this.b13);
+			this.Controls.Add(this.b12);
+			this.Controls.Add(this.b11);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "TikTakToe";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button b11;
+		private System.Windows.Forms.Button b12;
+		private System.Windows.Forms.Button b13;
+		private System.Windows.Forms.Button b21;
+		private System.Windows.Forms.Button b22;
+		private System.Windows.Forms.Button b23;
+		private System.Windows.Forms.Button b31;
+		private System.Windows.Forms.Button b32;
+		private System.Windows.Forms.Button b33;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button Reset;
+		private System.Windows.Forms.Button b;
+		private System.Windows.Forms.Button Ogra;
+		private System.Windows.Forms.Button graO;
+		private System.Windows.Forms.Button grazaX;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button grazaO;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label_chat;
+	}
+}
+
---
+++ TikTakTo_NN_49102/TikTakTo_NN_49102/Form1.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTakTo_NN_49102
+{
+	public partial class Form1 : Form
+	{
+		bool krok = true;// dla X - true dla O - false
+		int ilośćKroków = 0;
+		bool symbol;
+		bool znalazlem = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button_click(object sender, EventArgs e)
+		{
+			ilośćKroków++;
+			Button b = (Button)sender;
+			if (krok)
+			{
+				b.Text = "X";
+				b.BackColor = Color.Red;
+			}
+				
+			else
+			{
+				b.Text = "O";
+				b.BackColor = Color.Green;
+				
+			}
+			
+
+			krok = !krok;
+			Sprawdz();
+			if (krok != symbol)
+			  Komputerchodzi(szukaKomputer());
+			b.Enabled = false;
+			
+			
+			
+		}
+		 private void Sprawdz()
+		{
+			bool wygrywa = false;
+
+			//sprawdzamy poziomy 
+			if ((b11.Text == b12.Text) && (b12.Text == b13.Text) && (b11.Text == "X" || b11.Text == "O"))
+				wygrywa = true;
+			else if ((b21.Text == b22.Text) && (b22.Text == b23.Text) && (b21.Text == "X" || b21.Text == "O"))
+				wygrywa = true;
+			else if ((b31.Text == b32.Text) && (b32.Text == b33.Text) && (b31.Text == "X" || b31.Text == "O"))
+				wygrywa = true;
+
+			//sprawdzamy pionowo
+			else if((b11.Text == b21.Text) && (b21.Text == b31.Text) && (b11.Text == "X" || b11.Text == "O"))
+				wygrywa = true;
+			else if ((b12.Text == b22.Text) && (b22.Text == b32.Text) && (b12.Text == "X" || b12.Text == "O"))
+				wygrywa = true;
+			else if ((b13.Text == b23.Text) && (b23.Text == b33.Text) && (b13.Text == "X" || b13.Text == "O"))
+				wygrywa = true;
+
+			//sprawdzamy przekątna;
+			else if((b11.Text == b22.Text) && (b22.Text == b33.Text) && (b11.Text == "X" || b11.Text == "O"))
+				wygrywa = true;
+			else if ((b13.Text == b22.Text) && (b22.Text == b31.Text) && (b13.Text == "X" || b13.Text == "O"))
+				wygrywa = true;
+			
+
+
+			if (wygrywa)
+			{
+				disablebuttons();
+				string winner = "";
+				if (krok)
+					winner = "O";
+				else
+					winner = "X";
+				MessageBox.Show(winner + "  wygrywa ");
+			}
+			else
+			{
+				if (ilośćKroków == 9)
+					MessageBox.Show(" remis ");
+
+			}
+				
+
+		}
+		private void disablebuttons()
+		{
+
+			foreach (Component c in Controls)
+			{
+				if (c.GetType() == typeof(Button))
+				{
+					Button b = (Button)c;
+					b.Enabled = false;
+					Reset.Enabled = true;
+				}
+			}
+		
+		}
+
+		private void Reset_Click(object sender, EventArgs e)
+		{
+			foreach (Component c in Controls)
+			{
+				if (c.GetType() == typeof(Button))
+				{
+					Button b = (Button)c;
+					b.BackColor = Color.FromKnownColor(KnownColor.Control);
+					b.Enabled = false;
+					b.Text = "";
+				}
+			}
+				Reset.Text = "Reset";
+				krok = true;
+				ilośćKroków = 0;
+				grazaO.Enabled = true;
+				grazaX.Enabled = true;
+				Reset.Enabled = false;
+				grazaX.Text = "Gracz za X";
+				grazaO.Text = "Gracz za O";
+
+			
+		}
+		private void Komputerchodzi(in Button But)
+		{
+			string symbolKomputera, symbolgracza;
+			
+			if (symbol)
+			{
+				symbolKomputera = "O";
+				symbolgracza = "X";
+			}
+			else
+			{
+				symbolKomputera = "X";
+				symbolgracza = "O";
+			}
+			
+
+			if (SprawdżCzyKtośWygra(symbolgracza))
+				label_chat.Text = "O nie! Wygrasz w następnym ruchu";
+			if (ilośćKroków < 9)
+				But.PerformClick();
+			if (SprawdżCzyKtośWygra(symbolKomputera))
+				label_chat.Text = "Ha Ha ,W następnym ruchu wygram";
+
+
+
+		}
+		private Button szukaKomputer()
+		{
+			int i;
+			Button knopka = new Button();
+			Random rnd = new Random();
+			Button[] arraybut = {b11,b12,b13,b21,b22,b23,b31,b33,b32};
+			znalazlem = false;
+			string symbolKomputera, symbolgracza;
+			if (symbol)
+			{
+				symbolKomputera = "O";
+				symbolgracza = "X";
+			}
+			else
+			{
+				symbolKomputera = "X";
+				symbolgracza = "O";
+			}
+			
+
+			//komputer sprawdza mozliwość wygrać
+			//sprawdzamy poziomy 
+
+			if ((b11.Text == symbolKomputera) && (b12.Text == symbolKomputera) && (b13.Text == ""))
+				return(b13);
+			else if ((b11.Text == symbolKomputera) && (b12.Text == "") && (b13.Text == symbolKomputera))
+				return (b12);
+			else if ((b11.Text == "") && (b12.Text == symbolKomputera) && (b13.Text == symbolKomputera))
+				return (b11);
+
+			else if ((b21.Text == symbolKomputera) && (b22.Text == symbolKomputera) && (b23.Text == ""))
+				return (b23);
+			else if ((b21.Text == symbolKomputera) && (b22.Text == "") && (b23.Text == symbolKomputera))
+				return (b22);
+			else if ((b21.Text == "") && (b22.Text == symbolKomputera) && (b23.Text == symbolKomputera))
+				return (b21);
+
+			else if ((b31.Text == symbolKomputera) && (b32.Text == symbolKomputera) && (b33.Text == ""))
+				return (b33);
+			else if ((b31.Text == symbolKomputera) && (b32.Text == "") && (b33.Text == symbolKomputera))
+				return (b32);
+			else if ((b31.Text == "") && (b32.Text == symbolKomputera) && (b33.Text == symbolKomputera))
+				return (b31);
+
+			//sprawdzamy pionowo
+			else if ((b11.Text == symbolKomputera) && (b21.Text == symbolKomputera) && (b31.Text == ""))
+				return (b31);
+			else if ((b11.Text == symbolKomputera) && (b21.Text == "") && (b31.Text == symbolKomputera))
+				return (b21);
+			else if ((b11.Text == "") && (b21.Text == symbolKomputera) && (b31.Text == symbolKomputera))
+				return (b11);
+
+			else if ((b12.Text == symbolKomputera) && (b22.Text == symbolKomputera) && (b32.Text == ""))
+				return (b32);
+			else if ((b12.Text == symbolKomputera) && (b22.Text == "") && (b32.Text == symbolKomputera))
+				return (b22);
+			else if ((b12.Text == "") && (b22.Text == symbolKomputera) && (b32.Text == symbolKomputera))
+				return (b12);
+
+			else if ((b13.Text == symbolKomputera) && (b23.Text == symbolKomputera) && (b33.Text == ""))
+				return (b33);
+			else if ((b13.Text == symbolKomputera) && (b23.Text == "") && (b33.Text == symbolKomputera))
+				return (b23);
+			else if ((b13.Text == "") && (b23.Text == symbolKomputera) && (b33.Text == symbolKomputera))
+				return (b13);
+
+
+			//sprawdzamy przekątna;
+			else if ((b11.Text == symbolKomputera) && (b22.Text == symbolKomputera) && (b33.Text == ""))
+				return (b33);
+			else if ((b11.Text == symbolKomputera) && (b22.Text == "") && (b33.Text == symbolKomputera))
+				return (b22);
+			else if ((b11.Text == "") && (b22.Text == symbolKomputera) && (b33.Text == symbolKomputera))
+				return (b11);
+
+			else if ((b31.Text == symbolKomputera) && (b22.Text == symbolKomputera) && (b13.Text == ""))
+				return (b13);
+			else if ((b31.Text == symbolKomputera) && (b22.Text == "") && (b13.Text == symbolKomputera))
+				return (b22);
+			else if ((b31.Text == "") && (b22.Text == symbolKomputera) && (b13.Text == symbolKomputera))
+				return (b31);
+
+			//Komputer sprawdza możliwość bloku
+			//sprawdzamy poziomy 
+
+			if ((b11.Text == symbolgracza) && (b12.Text == symbolgracza) && (b13.Text == ""))
+				return (b13);
+			else if ((b11.Text == symbolgracza) && (b12.Text == "") && (b13.Text == symbolgracza))
+				return (b12);
+			else if ((b11.Text == "") && (b12.Text == symbolgracza) && (b13.Text == symbolgracza))
+				return (b11);
+
+			else if ((b21.Text == symbolgracza) && (b22.Text == symbolgracza) && (b23.Text == ""))
+				return (b23);
+			else if ((b21.Text == symbolgracza) && (b22.Text == "") && (b23.Text == symbolgracza))
+				return (b22);
+			else if ((b21.Text == "") && (b22.Text == symbolgracza) && (b23.Text == symbolgracza))
+				return (b21);
+
+			else if ((b31.Text == symbolgracza) && (b32.Text == symbolgracza) && (b33.Text == ""))
+				return (b33);
+			else if ((b31.Text == symbolgracza) && (b32.Text == "") && (b33.Text == symbolgracza))
+				return (b32);
+			else if ((b31.Text == "") && (b32.Text == symbolgracza) && (b33.Text == symbolgracza))
+				return (b31);
+
+			//sprawdzamy pionowo
+			else if ((b12.Text == symbolgracza) && (b21.Text == symbolgracza) && (b31.Text == ""))
+				return (b31);
+			else if ((b21.Text == symbolgracza) && (b21.Text == "") && (b31.Text == symbolgracza))
+				return (b12);
+			else if ((b12.Text == "") && (b21.Text == symbolgracza) && (b31.Text == symbolgracza))
+				return (b12);
+
+			else if ((b12.Text == symbolgracza) && (b22.Text == symbolgracza) && (b32.Text == ""))
+				return (b32);
+			else if ((b12.Text == symbolgracza) && (b22.Text == "") && (b32.Text == symbolgracza))
+				return (b22);
+			else if ((b12.Text == "") && (b22.Text == symbolgracza) && (b32.Text == symbolgracza))
+				return (b12);
+
+			else if ((b13.Text == symbolgracza) && (b23.Text == symbolgracza) && (b33.Text == ""))
+				return (b33);
+			else if ((b13.Text == symbolgracza) && (b23.Text == "") && (b33.Text == symbolgracza))
+				return (b23);
+			else if ((b13.Text == "") && (b23.Text == symbolgracza) && (b33.Text == symbolgracza))
+				return (b13);
+
+
+			//sprawdzamy przekątna;
+			else if ((b11.Text == symbolgracza) && (b22.Text == symbolgracza) && (b33.Text == ""))
+				return (b33);
+			else if ((b11.Text == symbolgracza) && (b22.Text == "") && (b33.Text == symbolgracza))
+				return (b22);
+			else if ((b11.Text == "") && (b22.Text == symbolgracza) && (b33.Text == symbolgracza))
+				return (b11);
+
+			else if ((b31.Text == symbolgracza) && (b22.Text == symbolgracza) && (b13.Text == ""))
+				return (b13);
+			else if ((b31.Text == symbolgracza) && (b22.Text == "") && (b13.Text == symbolgracza))
+				return (b22);
+			else if ((b31.Text == "") && (b22.Text == symbolgracza) && (b13.Text == symbolgracza))
+				return (b31);
+
+			//sprawdza czy centrum jest wolny
+			else if (b22.Text == "")
+			{
+				
+				label_chat.Text = "Zająłem środek";
+				return b22;
+			}
+			//sprawdza czy kąty są wolne
+			else if (b11.Text == "")
+				return b11;
+			else if (b31.Text == "")
+				return b31;
+			else if (b13.Text == "")
+				return b13;
+			else if (b33.Text == "")
+				return b33;
+
+			//szukamy wolne mejsce
+			else
+			{
+				foreach (Component c in Controls)
+				{
+					if (c.GetType() == typeof(Button))
+					{
+						Button b = (Button)c;
+						if (b.Text == "")
+							knopka = b;
+					}
+				}
+				return knopka;
+			}
+					
+
+
+
+
+
+		}
+
+		private void grazaX_Click(object sender, EventArgs e)
+		{
+			foreach (Component c in Controls)
+			{
+				if (c.GetType() == typeof(Button))
+				{
+					Button b = (Button)c;
+					b.BackColor = Color.FromKnownColor(KnownColor.Control);
+					b.Enabled = true;
+					b.Text = "";
+				}
+			}
+					Reset.Text = "Reset";
+					ilośćKroków = 0;
+					symbol = true;
+					grazaX.Text = "Gracz za X";
+					grazaO.Text = "Gracz za O";
+
+					grazaO.Enabled = false;
+					grazaX.Enabled = false;
+				
+			
+		}
+
+		private void grazaO_Click(object sender, EventArgs e)
+		{
+			foreach (Component c in Controls)
+			{
+
+				if (c.GetType() == typeof(Button))
+				{
+
+
+
+					Button b = (Button)c;
+					b.BackColor = Color.FromKnownColor(KnownColor.Control);
+					b.Enabled = true;
+					b.Text = "";
+				}
+			}
+					Reset.Text = "Reset";
+					ilośćKroków = 0;
+					symbol = false;
+
+					grazaO.Text = "Gracz za O";
+					grazaX.Text = "Gracz za X";
+					grazaO.Enabled = false;
+					grazaX.Enabled = false;
+				Komputerchodzi(szukaKomputer());
+		}
+
+		private bool SprawdżCzyKtośWygra(in string symbol)
+		{
+			//sprawdzamy poziomy 
+			int count = 0;
+			if ((b11.Text == symbol) && (b12.Text == symbol) && (b13.Text == ""))
+				count++;
+			 if ((b11.Text == symbol) && (b12.Text == "") && (b13.Text == symbol))
+				count++;
+			 if ((b11.Text == "") && (b12.Text == symbol) && (b13.Text == symbol))
+				count++;
+
+			 if ((b21.Text == symbol) && (b22.Text == symbol) && (b23.Text == ""))
+				count++;
+			 if ((b21.Text == symbol) && (b22.Text == "") && (b23.Text == symbol))
+				count++;
+			 if ((b21.Text == "") && (b22.Text == symbol) && (b23.Text == symbol))
+				count++;
+
+			 if ((b31.Text == symbol) && (b32.Text == symbol) && (b33.Text == ""))
+				count++;
+			 if ((b31.Text == symbol) && (b32.Text == "") && (b33.Text == symbol))
+				count++;
+			 if ((b31.Text == "") && (b32.Text == symbol) && (b33.Text == symbol))
+				count++;
+
+			//sprawdzamy pionowo
+			 if ((b11.Text == symbol) && (b21.Text == symbol) && (b31.Text == ""))
+				count++;
+			 if ((b11.Text == symbol) && (b21.Text == "") && (b31.Text == symbol))
+				count++;
+			 if ((b11.Text == "") && (b21.Text == symbol) && (b31.Text == symbol))
+				count++;
+			 if ((b12.Text == symbol) && (b22.Text == symbol) && (b32.Text == ""))
+				count++;
+			 if ((b12.Text == symbol) && (b22.Text == "") && (b32.Text == symbol))
+				count++;
+			 if ((b12.Text == "") && (b22.Text == symbol) && (b32.Text == symbol))
+				count++;
+			 if ((b13.Text == symbol) && (b23.Text == symbol) && (b33.Text == ""))
+				count++;
+			 if ((b13.Text == symbol) && (b23.Text == "") && (b33.Text == symbol))
+				count++;
+			 if ((b13.Text == "") && (b23.Text == symbol) && (b33.Text == symbol))
+				count++;
+
+			//sprawdzamy przekątna;
+			 if ((b11.Text == symbol) && (b22.Text == symbol) && (b33.Text == ""))
+				count++;
+			 if ((b11.Text == symbol) && (b22.Text == "") && (b33.Text == symbol))
+				count++;
+			 if ((b11.Text == "") && (b22.Text == symbol) && (b33.Text == symbol))
+				count++;
+			 if ((b31.Text == symbol) && (b22.Text == symbol) && (b13.Text == ""))
+				count++;
+			 if ((b31.Text == symbol) && (b22.Text == "") && (b13.Text == symbol))
+				count++;
+			 if ((b31.Text == "") && (b22.Text == symbol) && (b13.Text == symbol))
+				count++;
+
+			if (count > 1)
+				return true;
+			else return false;
+		}
+
+		
+	}
+}
